3ac9641521b4731573f9e1e97ef095909451810c
Kevin Le <iamkevinvle@gmail.com>
2023-05-07 14:07:06 -0700

    initial timer setup

--- src/features/timer/components/Timer.tsx
+++ src/features/timer/components/Timer.tsx
@@ -1,5 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Timer() {
-  return <>TIMER</>;
+  const minutesToSeconds = (minutes: number): number => {
+    return minutes * 60;
+  };
+  const [time, setTime] = useState(minutesToSeconds(25));
+  const minutes = Math.floor(time / 60);
+  const remainingSeconds = time - minutes * 60;
+  const formmatedTime = `${minutes}:${remainingSeconds}`;
+
+  useEffect(() => {
+    setInterval(() => {
+      setTime(time - 1);
+    }, 1000);
+  }, [time]);
+
+  return (
+    <>
+      <div>{formmatedTime}</div>
+    </>
+  );
 }
