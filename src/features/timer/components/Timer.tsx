766cdcb4be8fe43ba5eb107f2fc56b07a99d04fe
Kevin Le <iamkevinvle@gmail.com>
2023-05-07 20:58:36 -0700

    padding for timer

--- src/features/timer/components/Timer.tsx
+++ src/features/timer/components/Timer.tsx
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
 
+const minutesToSeconds = (minutes: number): number => {
+  return minutes * 60;
+};
+
+const formatTime = (timeInSeconds: number): string => {
+  const minutes = Math.floor(timeInSeconds / 60);
+  const remainingSeconds = timeInSeconds - minutes * 60;
+  const formattedMinutes = minutes.toString().padStart(2, "0");
+  const formattedSeconds = remainingSeconds.toString().padStart(2, "0");
+  return `${formattedMinutes}:${formattedSeconds}`;
+};
+
 export default function Timer() {
-  const minutesToSeconds = (minutes: number): number => {
-    return minutes * 60;
-  };
   const [time, setTime] = useState(minutesToSeconds(25));
-  const minutes = Math.floor(time / 60);
-  const remainingSeconds = time - minutes * 60;
-  const formmatedTime = `${minutes}:${remainingSeconds}`;
+  const formmatedTime = formatTime(time);
 
   useEffect(() => {
-    setInterval(() => {
-      setTime(time - 1);
+    const intervalId = setInterval(() => {
+      setTime((time) => time - 1);
     }, 1000);
-  }, [time]);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <>
