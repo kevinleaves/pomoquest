a93c86d997a4be9206fa640f4970f32507652957
Kevin Le <iamkevinvle@gmail.com>
2023-05-08 18:51:06 -0700

    add sound to button click

--- src/features/timer/components/Timer.tsx
+++ src/features/timer/components/Timer.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { ClassRegistry } from "superjson/dist/class-registry";
+import useSound from "use-sound";
 
 const minutesToSeconds = (minutes: number): number => {
   return minutes * 60;
@@ -40,16 +40,27 @@ export default function Timer() {
     return () => clearInterval(intervalId);
   }, [status]);
 
+  const StartButtonWithSound = () => {
+    const [play] = useSound("/ui-click.wav");
+    const handleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+      play();
+      toggleTimer();
+    };
+    return (
+      <button
+        onClick={handleClick}
+        className="grow rounded-2xl border p-2 shadow-md duration-200 hover:scale-125 hover:bg-purple-400 hover:text-white hover:shadow-xl"
+      >
+        {status ? "STOP" : "START"}
+      </button>
+    );
+  };
+
   return (
     <div className="flex h-80 w-5/6 max-w-md flex-col items-center justify-center gap-5 overflow-hidden rounded-3xl border p-24 md:w-1/2">
       <div className="text-8xl md:text-9xl">{formmatedTime}</div>
       <div className="flex w-full gap-5">
-        <button
-          onClick={toggleTimer}
-          className="grow rounded-2xl border p-2 shadow-md duration-200 hover:scale-125 hover:bg-purple-400 hover:text-white hover:shadow-xl"
-        >
-          {status ? "STOP" : "START"}
-        </button>
+        {StartButtonWithSound()}
         <button
           onClick={resetTimer}
           className="grow-0 rounded-2xl border p-2 shadow-md duration-200 hover:scale-125 hover:bg-purple-400 hover:text-white hover:shadow-xl"
