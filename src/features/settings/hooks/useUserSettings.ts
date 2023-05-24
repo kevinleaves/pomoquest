878308d1162ac53a5e39cb5d29d31e32058ad6df
Kevin Le <iamkevinvle@gmail.com>
2023-05-23 19:56:24 -0700

    refactor useUserSettings

--- src/features/settings/hooks/useUserSettings.ts
+++ src/features/settings/hooks/useUserSettings.ts
@@ -11,21 +11,20 @@ export default function useUserSettings() {
   const ctx = api.useContext();
 
   // queries
-  let { data: alarmSound, isLoading: isAlarmSoundLoading } =
+  const { data: alarmSound, isLoading: isAlarmSoundLoading } =
     api.settings.getCurrentAlarmSound.useQuery();
 
-  let { data: pomoDuration, isLoading: isPomoDurationLoading } =
+  const { data: pomoDuration, isLoading: isPomoDurationLoading } =
     api.settings.getPomoDuration.useQuery();
 
-  let { data: bgColor } = api.settings.getCurrentBgColor.useQuery();
+  const { data: bgColor } = api.settings.getCurrentBgColor.useQuery();
 
-  let { data: shortBreakDuration, isLoading: isSBreakLoading } =
+  const { data: shortBreakDuration, isLoading: isSBreakLoading } =
     api.settings.getShortBreakDuration.useQuery();
 
-  let { data: longBreakDuration, isLoading: isLBreakLoading } =
+  const { data: longBreakDuration, isLoading: isLBreakLoading } =
     api.settings.getLongBreakDuration.useQuery();
 
-  // mutations
   const { mutate: mutateBgColor } = api.settings.updateBgColor.useMutation({
     onSuccess: () => {
       void ctx.settings.getCurrentBgColor.invalidate();
@@ -36,26 +35,13 @@ export default function useUserSettings() {
     mutateBgColor({ hexValue });
   };
 
-  // if any query is loading,
-  isAlarmSoundLoading = isAlarmSoundLoading;
-  isPomoDurationLoading = isPomoDurationLoading;
-  isSBreakLoading = isSBreakLoading;
-  isLBreakLoading = isLBreakLoading;
-
-  // handle undefined w/ nullish coalescing operator
-  bgColor = bgColor ?? "";
-  alarmSound = alarmSound ?? "";
-  pomoDuration = pomoDuration ?? 25;
-  shortBreakDuration = shortBreakDuration ?? 5;
-  longBreakDuration = longBreakDuration ?? 15;
-
   return {
     data: {
-      bgColor,
-      alarmSound,
-      pomoDuration,
-      shortBreakDuration,
-      longBreakDuration,
+      bgColor: bgColor ?? "",
+      alarmSound: alarmSound ?? "",
+      pomoDuration: pomoDuration ?? 25,
+      shortBreakDuration: shortBreakDuration ?? 5,
+      longBreakDuration: longBreakDuration ?? 15,
     },
     mutations: {
       updateBgColor,
