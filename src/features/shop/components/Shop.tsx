8cfc7043a5dd35d902ce05978abb7d8813d28c56
Kevin Le <iamkevinvle@gmail.com>
2023-05-31 19:43:28 -0700

    on shop click purchase item which removes it from shop view

--- src/features/shop/components/Shop.tsx
+++ src/features/shop/components/Shop.tsx
@@ -1,14 +1,10 @@
 import React, { useState } from "react";
 import { api } from "~/utils/api";
+import type { Unlockable } from "@prisma/client";
 
 interface Props {
   isShopOpen: boolean;
   off: () => void;
-}
-
-function buyItem() {
-  // logic to buy an item
-  console.log("bought!");
 }
 
 export default function Shop({ isShopOpen, off }: Props) {
@@ -16,8 +12,31 @@ export default function Shop({ isShopOpen, off }: Props) {
   const { data: purchasableBGColors, isLoading } =
     api.unlockedSettings.getLockedBGColors.useQuery();
 
+  const { mutate: purchaseItem, isLoading: isPurchasing } =
+    api.unlockedSettings.purchaseItem.useMutation();
+
+  const { mutate: subtractCoins } = api.coins.subtractCoins.useMutation();
+
+  function buyItem(item: Unlockable) {
+    // logic to buy an item
+    console.log("bought!");
+    /*
+    on purchase click,
+    grab row's ID
+    update purchased to true
+    grab item's cost and call removed coins route
+    invalidate get queries for shop & settings to "refresh" the page
+    */
+    const itemId = item.id;
+    const amount = Number(item.cost);
+
+    purchaseItem({ itemId });
+    //purchase only if user has >= amount of coins needed for the purchase
+    subtractCoins({ amount });
+  }
+
   return isShopOpen ? (
-    <div className="rounded-xl bg-white p-10 md:fixed md:left-1/2 md:top-1/2 md:h-3/4 md:w-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+    <div className="rounded-xl bg-white p-10 font-archivo md:fixed md:left-1/2 md:top-1/2 md:h-3/4 md:w-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
       <div className="text-center font-publicSans text-3xl font-bold">SHOP</div>
       <button
         className="absolute right-0 top-0 text-4xl transition hover:scale-110"
@@ -26,23 +45,25 @@ export default function Shop({ isShopOpen, off }: Props) {
         close
       </button>
       <div className="flex h-5/6 flex-col items-center gap-2 rounded-xl">
-        {purchasableBGColors?.map((obj, index) => (
+        {purchasableBGColors?.map((item, index) => (
           <div
             key={index}
             onClick={() => setCurrentlySelected(index)}
             className={`flex w-5/6 justify-between ${
-              index === currentlySelected ? "rounded-xl border-2" : ""
+              index === currentlySelected
+                ? "rounded-xl border-2 bg-gray-900 p-4 text-white"
+                : ""
             }`}
           >
             <button
               className="h-20 w-20 rounded-lg"
-              style={{ backgroundColor: obj.value }}
+              style={{ backgroundColor: item.value }}
             ></button>
             <button
               className="transition hover:text-red-500"
-              onClick={() => buyItem()}
+              onClick={() => buyItem(item)}
             >
-              {obj.cost} coins
+              {item.cost} coins
             </button>
           </div>
         ))}
