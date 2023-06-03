58d5f3236c911186705d735d66a48a72e33b741a
Kevin Le <iamkevinvle@gmail.com>
2023-06-03 15:28:45 -0700

    settings styling changes

--- src/features/settings/components/Settings.tsx
+++ src/features/settings/components/Settings.tsx
@@ -16,8 +16,8 @@ export default function Settings({
     api.unlockedSettings.getUnlockedBGColors.useQuery();
 
   return isUserSettingsModalOpen ? (
-    <div className="rounded-xl bg-white p-10 md:fixed md:left-1/2 md:top-1/2 md:h-3/4 md:w-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
-      <div className="text-8xl">IM A SETTINGS MODAL AND IM OPEN</div>
+    <div className="h-full rounded-xl bg-white p-10 font-publicSans md:fixed md:left-1/2 md:top-1/2 md:h-3/4 md:w-1/2 md:-translate-x-1/2 md:-translate-y-1/2">
+      <div className="text-2xl font-bold">Settings</div>
       <h3 className="text-xl">theme</h3>
       <div className="flex gap-2">
         {possibleBGColors?.map((color) => (
@@ -34,7 +34,7 @@ export default function Settings({
       <div className="flex"></div>
 
       <button
-        className="absolute right-0 top-0 text-4xl transition hover:scale-110"
+        className="absolute right-5 top-3  text-2xl  font-bold transition hover:scale-110"
         onClick={off}
       >
         close
