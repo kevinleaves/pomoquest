c8190beef06f50ff124610dbf4cfc5f339c64be2
Kevin Le <iamkevinvle@gmail.com>
2023-06-21 13:33:05 -0700

    rate limit purchasing

--- src/server/api/routers/coins.ts
+++ src/server/api/routers/coins.ts
@@ -1,7 +1,21 @@
 import { z } from "zod";
-import { clerkClient } from "@clerk/nextjs/server";
-
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
+import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 1 requests per 10 seconds. prevents double click on 1 shop item
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(2, "10 s"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 export const coinsRouter = createTRPCRouter({
   getCoins: privateProcedure.query(async ({ ctx }) => {
@@ -58,28 +72,37 @@ export const coinsRouter = createTRPCRouter({
     .mutation(async ({ ctx, input }) => {
       const userID = ctx.currentUser;
       const { amount } = input;
-      const user = await ctx.prisma.user.findUnique({
-        where: {
-          id: userID,
-        },
-        select: {
-          coins: true,
-        },
-      });
+      const { success } = await ratelimit.limit(userID);
 
-      if (user && user.coins >= amount) {
-        await ctx.prisma.user.update({
+      if (success) {
+        const user = await ctx.prisma.user.findUnique({
           where: {
             id: userID,
           },
-          data: {
-            coins: {
-              decrement: amount,
-            },
+          select: {
+            coins: true,
           },
         });
+
+        if (user && user.coins >= amount) {
+          await ctx.prisma.user.update({
+            where: {
+              id: userID,
+            },
+            data: {
+              coins: {
+                decrement: amount,
+              },
+            },
+          });
+        } else {
+          throw new Error("Insufficient coins.");
+        }
       } else {
-        throw new Error("Insufficient coins.");
+        throw new TRPCError({
+          code: "TOO_MANY_REQUESTS",
+          message: "Too many requests. Try again in a bit.",
+        });
       }
     }),
 });
--- src/server/api/routers/unlockedSettings.ts
+++ src/server/api/routers/unlockedSettings.ts
@@ -1,6 +1,21 @@
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
+
+// Create a new ratelimiter, that allows 1 requests per 10 seconds. prevents double click on 1 shop item
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(2, "10 s"),
+  analytics: true,
+  /**
+   * Optional prefix for the keys used in redis. This is useful if you want to share a redis
+   * instance with other applications and want to avoid key collisions. The default prefix is
+   * "@upstash/ratelimit"
+   */
+  prefix: "@upstash/ratelimit",
+});
 
 export const unlockedSettingsRouter = createTRPCRouter({
   getUnlockedBGColors: privateProcedure.query(async ({ ctx }) => {
@@ -44,38 +59,46 @@ export const unlockedSettingsRouter = createTRPCRouter({
     )
     .mutation(async ({ ctx, input }) => {
       const userID = ctx.currentUser;
+      const { success } = await ratelimit.limit(userID);
+
       const { itemId } = input;
-      const user = await ctx.prisma.user.findUnique({
-        where: {
-          id: userID,
-        },
-        select: {
-          coins: true,
-        },
-      });
 
-      const item = await ctx.prisma.unlockable.findUnique({
-        where: {
-          id: itemId,
-        },
-        select: {
-          cost: true,
-        },
-      });
-
-      if (user && item && user.coins >= item.cost) {
-        await ctx.prisma.unlockable.update({
+      if (success) {
+        const user = await ctx.prisma.user.findUnique({
+          where: {
+            id: userID,
+          },
+          select: {
+            coins: true,
+          },
+        });
+        const item = await ctx.prisma.unlockable.findUnique({
           where: {
             id: itemId,
           },
-          data: {
-            purchased: true,
+          select: {
+            cost: true,
           },
         });
+        if (user && item && user.coins >= item.cost) {
+          await ctx.prisma.unlockable.update({
+            where: {
+              id: itemId,
+            },
+            data: {
+              purchased: true,
+            },
+          });
+        } else {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "Insufficient coins.",
+          });
+        }
       } else {
         throw new TRPCError({
-          code: "INTERNAL_SERVER_ERROR",
-          message: "Insufficient coins.",
+          code: "TOO_MANY_REQUESTS",
+          message: "Too many requests. Try again in a bit.",
         });
       }
     }),
